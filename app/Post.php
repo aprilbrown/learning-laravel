335d358ae166c3097d57edd0b719d6ecc86df905
April Brown <april.brown@clearlink.com>
2017-04-07 15:46:50 -0600

    Learned posting and other elements in todays learning.

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
-    //
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>A Place to show the posts.</h1>
+    <div class="col-sm-8 blog-main">
+        <h1>{{ $post->title }}</h1>
+
+        {{ $post->body }}
+
+        <hr>
+
+        <div class="comments">
+            <ul class="list-group">
+            @foreach($post->comments as $comment)
+                <li class="list-group-item">
+                    <strong>
+                        {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                    </strong>
+                    {{ $comment->body }}
+                </li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
+
+
 @endsection
