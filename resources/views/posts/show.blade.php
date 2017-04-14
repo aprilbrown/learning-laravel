84ce8b20113fc571b98b61b43fc6537408f93eee
April Brown <april.brown@clearlink.com>
2017-04-14 12:11:19 -0600

    Creating Login and logout ability and associating posts with users.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -20,7 +20,24 @@
             @endforeach
             </ul>
         </div>
+
+        <hr>
+
+        <div class="card">
+            <div class="card-block">
+                <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your Comment Here." class="form-control" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </div>
+                </form>
+
+                @include ('layouts.errors')
+            </div>
+        </div>
     </div>
-
-
 @endsection
