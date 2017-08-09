87b039bb01be3dac39f18a2ca28e0d6c1fc20f15
April Brown <april.brown@clearlink.com>
2017-08-09 12:53:05 -0600

    making home link return to main page

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,13 +1,13 @@
 <div class="blog-masthead">
     <div class="container">
         <nav class="nav blog-nav">
-            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link active" href="/">Home</a>
             @if (Auth::check())
                 <a class="nav-link" href="/posts/create">New Post</a>
             @endif
 
             @if (Auth::check())
-                <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
+                <a class="nav-link ml-auto" href="/">{{ Auth::user()->name }}</a>
             @else
                 <a class="nav-link ml-auto" href="/login">Login</a>
                 <a class="nav-link" href="/register">Register</a>
