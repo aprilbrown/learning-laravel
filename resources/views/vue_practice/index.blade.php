c5dd7ff78738c5939ffc512a7a55ae685ef30063
April Brown <april.brown@clearlink.com>
2017-08-30 23:09:36 -0600

    Started next lesson.

--- resources/views/vue_practice/index.blade.php
+++ resources/views/vue_practice/index.blade.php
@@ -4,6 +4,7 @@
     <style>
         .is-loading{ background: #5cb85c;}
     </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.css"/>
     <script src="https://unpkg.com/vue@2.4.2"></script>
     <div class="col-sm-8 blog-main">
         <div id="lesson1">
@@ -45,6 +46,17 @@
             <task-list></task-list>
         </div>
         <hr>
+        <div id="lesson9" class="containernp">
+            <article class="message">
+                <div class="message-header">
+                    <p>Hello World</p>
+                    <button class="delete" aria-label="delete"></button>
+                </div>
+                <div class="message-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+                </div>
+            </article>
+        </div>
     </div>
 
     <script src="/js/main.js"></script>
