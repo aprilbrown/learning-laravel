2044b1309aef01d56ec4d58056b8ff0dd6fb2945
April Brown <april.brown@clearlink.com>
2017-09-06 17:01:02 -0600

    Up to lesson 10 down on vue Laracast vids.

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,38 @@ Vue.component('task-list',{
 Vue.component('task',{
     template: '<li><slot></slot></li>'
 });
-
+Vue.component('message',{
+    props: ['title', 'body'],
+    data(){
+        return{
+         isVisible: true
+        };
+    }    ,
+    template: `
+                <article class="message" v-show="isVisible">
+                <div class="message-header">
+                    {{ title }}
+                    <button class="delete" aria-label="delete" @click="isVisible = false"></button>
+                </div>
+                <div class="message-body">
+                    {{ body }}
+                </div>
+            </article>
+    `
+});
+Vue.component('modal', {
+    template: `
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot></slot>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+        </div>
+    </div>
+    `
+})
 
 var lesson1 = new Vue({
     el: '#lesson1',
@@ -31,7 +62,7 @@ var lesson3 = new Vue({
         names: ['Joe', 'Mary', 'Jane', 'Jack']
     },
     methods: {
-        addName: function(){
+        addName(){
             this.names.push(this.newName);
             this.newName = '';
         }
@@ -44,7 +75,7 @@ var lesson5 = new Vue({
         isLoading: false
     },
     methods: {
-        toggleClass: function(){
+        toggleClass(){
             this.isLoading = true;
         }
     }
@@ -68,10 +99,18 @@ var lesson6 = new Vue({
         }
     }
 })
-
 var lesson7 = new Vue({
     el: '#lesson7'
 })
 var lesson8 = new Vue({
     el: '#lesson8'
+})
+var lesson9 = new Vue({
+    el: '#lesson9'
+})
+var lesson10 = new Vue({
+    el: '#lesson10',
+    data: {
+        showModal: false
+    }
 })
--- resources/views/vue_practice/index.blade.php
+++ resources/views/vue_practice/index.blade.php
@@ -46,16 +46,22 @@
             <task-list></task-list>
         </div>
         <hr>
-        <div id="lesson9" class="containernp">
-            <article class="message">
-                <div class="message-header">
-                    <p>Hello World</p>
-                    <button class="delete" aria-label="delete"></button>
-                </div>
-                <div class="message-body">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-                </div>
-            </article>
+        <div id="lesson9">
+
+            <message title="Hellow World" body="Lorem ipsum dolar sit amet."></message>
+            <message title="Join Me" body="Sup dog?"></message>
+
+        </div>
+        <hr>
+        <div id="lesson10">
+            <modal v-if="showModal" @close="showModal = false">
+                <p>Bacon ipsum dolor amet rump short ribs ham brisket strip steak shankle. Pork loin corned beef ham biltong. Pancetta flank swine kevin brisket doner pork chop, spare ribs beef frankfurter pork beef ribs rump. Tongue frankfurter shoulder short ribs.</p>
+
+                <br/>
+
+                <p>Porchetta biltong shank venison picanha boudin pastrami. Meatball tenderloin swine meatloaf chuck beef kevin brisket pork belly burgdoggen jerky cupim shankle. Brisket tongue venison, fatback pork chop pork short ribs pig doner swine. Salami biltong prosciutto, spare ribs kevin fatback ribeye kielbasa doner. Corned beef meatloaf venison prosciutto, porchetta shoulder t-bone shank shankle capicola. Beef spare ribs swine filet mignon rump venison salami. Short ribs chicken picanha kielbasa strip steak alcatra.</p>
+            </modal>
+            <button @click="showModal = true">Show Modal</button>
         </div>
     </div>
 
